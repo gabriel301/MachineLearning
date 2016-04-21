221bf1fba8b75db936651e1a5cc8361d0d1d9414
Gabriel Augusto <gabriel301@gmail.com>
2016-04-21 12:28:11 -0300

    Assignment 6: SVM and Spam Classifier

---
+++ Coursera Andrew NG Course/Assignment 6/src/dataset3Params.m
@@ -0,0 +1,53 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.1;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+  %% Find the better combination of C and Sigma (C = 1 and sigma = 0.1)
+%      currentError = 999999;
+%      values = [0.01,0.03,0.1,0.3,1,3,10,30];
+%      l = length(values);
+%      for i = 1:l
+%        currentC = values(i);
+%        for j=1:l
+%          currentSigma = values(j);
+%          model= svmTrain(X, y, currentC, @(x1, x2) gaussianKernel(x1, x2, currentSigma)); 
+%          predictions = svmPredict(model, Xval);
+%          errorPrediction = mean(double(predictions ~= yval));
+%          if currentError > errorPrediction 
+%            currentError = errorPrediction;
+%            C = currentC;
+%            sigma = currentSigma;
+%            fprintf("C: %f; Sigma: %f", C, sigma);
+%          end
+%        end
+%      end  
+
+
+
+
+
+
+% =========================================================================
+
+end
